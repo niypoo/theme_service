847421bb145e9224a784a5745fec21ade0edba60
Mahmoud Nabhan <niypoo@gmail.com>
2023-10-26 15:14:57 +0300

    Update theme.service.dart

--- lib/theme.service.dart
+++ lib/theme.service.dart
@@ -2,77 +2,26 @@ import 'package:get/get.dart';
 import 'package:flutter/material.dart';
 import 'package:local_storage_service/localStorage.service.dart';
 import 'package:theme_service/models/theme.model.dart';
-import 'package:theme_service/theme.helper.dart';
 
 class ThemeService extends GetxService {
   //share controller in to
   static ThemeService get to => Get.find();
 
-  final String fontName;
-  final Color primary;
-  final Color secondary;
+  //map of themes light/dark
+  final List<ThemeX> themes;
 
   // constructor
   ThemeService({
-    required this.fontName,
-    required this.secondary,
-    required this.primary,
+    required this.themes,
   });
 
   //current form settings table  // default is auto
   String? currentTheme;
 
-  //map of themes light/dark
-  final List<ThemeX> themes = [];
-
-  Future<ThemeService> init({
-    List<ThemeX> additionalThemes = const [],
-  }) async {
+  Future<ThemeService> init() async {
     //get theme from locale storage
     currentTheme =
         currentTheme = LocaleStorageService.to.instance.read('theme') ?? 'Auto';
-
-    // Inject default themes Light/Dark
-    themes.addAll(
-      [
-        ThemeX(
-          name: 'Light',
-          theme: ThemeDataHelper.create(
-            ThemeData.light(),
-            fontName: fontName,
-            primaryColor: primary,
-            secondaryColor: secondary,
-            scaffoldBackgroundColor: Colors.white,
-            cardColor: const Color(0xffF4F4F4),
-            appBarColor: Colors.white,
-            brightness: Brightness.light,
-            fontColor: const Color(0xff333333),
-            secondaryTextColor: Colors.white,
-            iconColor: const Color(0xff333333),
-            secondaryIconColor: Colors.white,
-          ),
-        ),
-        ThemeX(
-          name: 'Dark',
-          theme: ThemeDataHelper.create(
-            ThemeData.dark(),
-            fontName: fontName,
-            primaryColor: primary,
-            secondaryColor: secondary,
-            brightness: Brightness.dark,
-            scaffoldBackgroundColor: const Color(0xff040404),
-            appBarColor: const Color(0xff040404),
-            cardColor: const Color(0xff161616),
-            fontColor: Colors.grey[500],
-            iconColor: Colors.grey[500],
-            secondaryTextColor: Colors.grey[200],
-            secondaryIconColor: Colors.grey[200],
-          ),
-        ),
-        // inject more themes
-        ...additionalThemes
-      ],
-    );
 
     return this;
   }
