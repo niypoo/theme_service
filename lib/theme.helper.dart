d305177909a64a7bce2a243902e06d87ed14e71b
Mahmoud Nabhan <niypoo@gmail.com>
2023-10-26 14:52:27 +0300

    apply responsive

--- lib/theme.helper.dart
+++ lib/theme.helper.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:get/get.dart';
+import 'package:responsive_helper/extensions/responsive.extension.dart';
 
 class ThemeDataHelper {
   static ThemeData create(
@@ -38,31 +38,105 @@ class ThemeDataHelper {
       textButtonTheme: TextButtonThemeData(
         style: ButtonStyle(
           textStyle: MaterialStateProperty.all(
-            TextStyle(color: primaryColor),
+            TextStyle(color: primaryColor, fontSize: 18.sp),
           ),
         ),
       ),
       elevatedButtonTheme: ElevatedButtonThemeData(
         style: ButtonStyle(
           textStyle: MaterialStateProperty.all(
-            TextStyle(color: primaryColor),
+            TextStyle(color: primaryColor, fontSize: 18.sp),
           ),
         ),
       ),
       cupertinoOverrideTheme: CupertinoThemeData(
         textTheme: CupertinoTextThemeData(
           dateTimePickerTextStyle: TextStyle(
-            fontSize: 17,
+            fontSize: 13.sp,
             fontFamily: fontName,
             color: fontColor,
           ),
         ),
       ),
-      iconTheme: IconThemeData(color: iconColor, size: iconSize),
-      textTheme: Get.textTheme.apply(
-        fontFamily: fontName,
-        bodyColor: fontColor,
-        displayColor: fontColor,
+      iconTheme: IconThemeData(color: iconColor, size: 16.sp),
+      // textTheme: Get.textTheme.apply(
+      //   fontFamily: fontName,
+      //   bodyColor: fontColor,
+      //   displayColor: fontColor,
+      // ),
+      textTheme: TextTheme(
+        headlineLarge: TextStyle(
+          fontFamily: fontName,
+          color: fontColor,
+          fontWeight: FontWeight.w900,
+          fontSize: 22.sp,
+        ),
+        headlineMedium: TextStyle(
+          fontFamily: fontName,
+          color: fontColor,
+          fontWeight: FontWeight.w900,
+          fontSize: 20.sp,
+        ),
+        headlineSmall: TextStyle(
+          fontFamily: fontName,
+          color: fontColor,
+          fontWeight: FontWeight.w800,
+          fontSize: 18.sp,
+        ),
+        titleLarge: TextStyle(
+          fontFamily: fontName,
+          color: fontColor,
+          fontWeight: FontWeight.w700,
+          fontSize: 16.sp,
+        ),
+        titleMedium: TextStyle(
+          fontFamily: fontName,
+          color: fontColor,
+          fontWeight: FontWeight.w600,
+          fontSize: 14.sp,
+        ),
+        titleSmall: TextStyle(
+          fontFamily: fontName,
+          color: fontColor,
+          fontWeight: FontWeight.w500,
+          fontSize: 12.sp,
+        ),
+        bodyLarge: TextStyle(
+          fontFamily: fontName,
+          color: fontColor,
+          fontWeight: FontWeight.w300,
+          fontSize: 18.sp,
+        ),
+        bodyMedium: TextStyle(
+          fontFamily: fontName,
+          color: fontColor,
+          fontWeight: FontWeight.w300,
+          fontSize: 16.sp,
+        ),
+        bodySmall: TextStyle(
+          fontFamily: fontName,
+          color: fontColor,
+          fontWeight: FontWeight.w300,
+          fontSize: 14.sp,
+        ),
+        labelLarge: TextStyle(
+          fontFamily: fontName,
+          color: fontColor,
+          fontWeight: FontWeight.w300,
+          fontSize: 14.sp,
+        ),
+        labelMedium: TextStyle(
+          fontFamily: fontName,
+          color: fontColor,
+          fontWeight: FontWeight.w200,
+          fontSize: 12.sp,
+        ),
+        labelSmall: TextStyle(
+          fontFamily: fontName,
+          color: fontColor,
+          fontWeight: FontWeight.w100,
+          fontSize: 10.sp,
+        ),
       ),
       bottomNavigationBarTheme: BottomNavigationBarThemeData(
         elevation: 2,
@@ -76,6 +150,12 @@ class ThemeDataHelper {
         color: appBarColor,
         iconTheme: IconThemeData(color: iconColor, size: iconSize),
         actionsIconTheme: IconThemeData(color: iconColor),
+        titleTextStyle: TextStyle(
+          fontFamily: fontName,
+          color: fontColor,
+          fontWeight: FontWeight.w900,
+          fontSize: 20.sp,
+        ),
       ),
     );
   }
