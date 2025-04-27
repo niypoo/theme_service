958622146bbd29abd024667b48a9e74951491281
Mahmoud Nabhan <niypoo@gmail.com>
2025-04-27 17:20:10 +0300

    remove patata_responsive

--- lib/theme.helper.dart
+++ lib/theme.helper.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:patata_responsive/patata_responsive.dart';
+import 'package:fly_ui/extensions/responsive.extension.dart';
 
 class ThemeDataHelper {
   static ThemeData create(
